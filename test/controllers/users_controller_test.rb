d2c4e83ea193125ce2dfaecdf7e0af339792a0fa
hayate <satz-03130@outlook.jp>
2025-05-10 15:56:59 +0900

    fix:test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,10 +3,12 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
    def setup
     User.delete_all # 既存のユーザーを削除
+    @unique_email = "test_#{SecureRandom.hex(10)}@example.com" # ランダムなメールアドレス
+    @user = User.create!(email: @unique_email, password: "password") # ユーザーを作成
   end
 
   test "should get show" do
-    get users_show_url
+    get users_show_url(@user) # ユーザーのIDを渡す
     assert_response :success
   end
 end
