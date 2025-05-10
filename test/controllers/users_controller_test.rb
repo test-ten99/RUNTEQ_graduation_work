e3a386727a432798d8f616e266140a13b8e67017
hayate <satz-03130@outlook.jp>
2025-05-10 15:53:16 +0900

    fix:test

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+   def setup
+    User.delete_all # 既存のユーザーを削除
+  end
+
   test "should get show" do
     get users_show_url
     assert_response :success
