ac6ff7f3db1259a41a4188ccd36726b836ecd9d7
hayate <satz-03130@outlook.jp>
2025-05-23 20:42:11 +0900

    fix:fly用にspotify修正

--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -1,1 +1,7 @@
-RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
+# if Rails.env.production? && ENV["FLY"] == "true"
+#   RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_CLIENT_SECRET'])
+# end
+
+if Rails.env.production? && ENV["SPOTIFY_CLIENT_ID"].present? && ENV["SPOTIFY_CLIENT_SECRET"].present?
+  RSpotify::authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+end
