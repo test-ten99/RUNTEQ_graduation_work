1e7feb3f3dd40589f4c6dc3be782359360dafde9
hayate <satz-03130@outlook.jp>
2025-05-24 14:47:16 +0900

    fix:lint

--- Gemfile
+++ Gemfile
@@ -61,4 +61,4 @@ end
 gem "devise"
 gem "rails-ujs"
 gem "dockerfile-rails", ">= 1.7", group: :development
-gem 'rspotify'
+gem "rspotify"
--- config/initializers/rspotify.rb
+++ config/initializers/rspotify.rb
@@ -7,7 +7,7 @@
 # end
 
 if ENV["SPOTIFY_CLIENT_ID"].present? && ENV["SPOTIFY_CLIENT_SECRET"].present?
-  RSpotify::authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
+  RSpotify.authenticate(ENV["SPOTIFY_CLIENT_ID"], ENV["SPOTIFY_CLIENT_SECRET"])
 else
   Rails.logger.warn("Spotify credentials are missing. RSpotify not authenticated.")
 end
